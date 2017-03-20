0ff6b9cc764a3e0cc1a52b76ee1521f8c6c5fd10
ncc <ncc>
2017-03-20 10:21:24 -0700

    ToDo Update: HashTable 2

--- MapExample/Map.cpp
+++ MapExample/Map.cpp
@@ -17,14 +17,30 @@ Map::~Map()
 
 void Map::Move(Location *newLocation) 
 {	
-	std::string checkNorth = std::to_string(newLocation->x)+ "," + std::to_string(newLocation->y + 1);
-	std::string checkSouth = std::to_string(newLocation->x) + "," + std::to_string(newLocation->y - 1);
-	std::string checkEast = std::to_string(newLocation->x + 1) + "," + std::to_string(newLocation->y);
-	std::string checkWest = std::to_string(newLocation->x - 1) + "," + std::to_string(newLocation->y);
+	std::string checkNorth = std::to_string(newLocation->x)
+		+ "," + std::to_string(newLocation->y + 1);
+	std::string checkSouth = std::to_string(newLocation->x) 
+		+ "," + std::to_string(newLocation->y - 1);
+	std::string checkEast = std::to_string(newLocation->x + 1) 
+		+ "," + std::to_string(newLocation->y);
+	std::string checkWest = std::to_string(newLocation->x - 1) 
+		+ "," + std::to_string(newLocation->y);
 
 	if (_HashTable.count(checkNorth) == 1)
 	{
 		newLocation->North = _HashTable.at(checkNorth);
+	}
+	if (_HashTable.count(checkSouth) == 1)
+	{
+		newLocation->South = _HashTable.at(checkSouth);
+	}
+	if (_HashTable.count(checkEast) == 1)
+	{
+		newLocation->East = _HashTable.at(checkEast);
+	}
+	if (_HashTable.count(checkWest) == 1)
+	{
+		newLocation->West = _HashTable.at(checkWest);
 	}
 	_locationsVisited.push(newLocation);
 	CurrentLocation = newLocation;
